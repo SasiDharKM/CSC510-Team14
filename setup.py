75d75ec25362cf0aa6eca33afce9a6dc02631513
Chaitanya Bandikatla <chaitanyab19952007@gmail.com>
2020-08-12 22:18:41 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='CSC510-Team14',
+    version='0.1',
+    description='Module for CSC510 - Fall20 by team Team 14',
+    author='',
+    author_email='',
+    url='',
+    packages=['spatialtree'],
+      long_description="""     TBD      """,
+      classifiers=[
+          "License :: MIT License",
+          "Programming Language :: Python",
+          "Development Status :: TBD",
+          "Intended Audience :: Developers",
+          "Topic :: Semester Project",
+      ],
+      keywords='TBD',
+      license='mit',
+      install_requires=[],
+      )
