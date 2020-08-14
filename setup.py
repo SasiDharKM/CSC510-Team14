074f104217b9c583c09168e62fbe9652a8061f98
Chaitanya Bandikatla <chaitanyab19952007@gmail.com>
2020-08-14 13:19:44 -0400

    Cbandik/reorg (#2)
    Reorg of folders and changes to gitignore and setup.py
    
    Co-authored-by: Chaitanya Bandikatla <cbandik@ncsu.edu>

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='CSC510-Team14',
+    name='GameOfLife',
     version='0.1',
     description='Module for CSC510 - Fall20 by team Team 14',
     author='CSC510-Team14',
     author_email='cbandik@ncsu.edu',
     url='https://github.com/SasiDharKM/CSC510-Team14',
-    packages=['CSC510-Team14'],
+    packages=['code'],
       long_description=""" Repository that holds the code for CSC 510 project team 14 """,
       classifiers=[
           "License :: MIT License",
